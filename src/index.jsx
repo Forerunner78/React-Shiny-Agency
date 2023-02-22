8f0c8162111fc7d73672f47bd5c0958bb6c57d9b
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-02-22 17:19:54 +0100

    git commit 'integrate P1C4 solution fix'

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Homepage from './pages/Home/Homepage'
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+    * {
+		font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+		margin: 0;
+    }
+	div {
+		margin: 20px;
+	}
+`
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Homepage />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<GlobalStyle />
+			<Homepage />
+		</BrowserRouter>
+	</React.StrictMode>
 )
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -5,35 +5,88 @@ import Error from '../../components/Error/Error'
 import Survey from '../Survey/Survey'
 import Results from '../Results/Results'
 import Freelances from '../Freelances/Freelances'
+import Presentation from '../../assets/home-illustration.svg'
+import colors from "../../utils/style/colors";
+import styled from "styled-components";
+import { StyledLink } from "../../utils/style/Atoms";
+
+
+const HomeWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	background-color: grey;
+`
+
+const HomerContainer = styled.div`
+	margin: 30px;
+	background-color: ${colors.background};
+	padding: 60px 90px;
+	display: flex;
+	flex-direction: row;
+	max-width: 1200px;
+`
+
+const LeftCol = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	flex: 1;
+	padding-right: 60px;
+	${StyledLink} {
+		max-width: 250px;
+	}
+`
+
+const StyledTitle = styled.h2`
+	padding-bottom: 30px;
+	max-width: 280px;
+	line-height: 50px;
+`
+
+const Illustration = styled.img`
+	flex: 1;
+	padding-left: 60px
+`
 
 export default function Homepage() {
-  return (
-    <div>
+	return (
+		<div>
 
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="survey/:questionNumber" element={<Survey />} />
-          <Route path="results" element={<Results />} />
-          <Route path="freelances" element={<Freelances />} />
+		{/* Routes nest inside one another. Nested route paths build upon
+				parent route paths, and nested route elements render inside
+				parent route elements. See the note about <Outlet> below. */}
+		<Routes>
+			<Route path="/" element={<Header />}>
+			<Route index element={<Home />} />
+			<Route path="survey/:questionNumber" element={<Survey />} />
+			<Route path="results" element={<Results />} />
+			<Route path="freelances" element={<Freelances />} />
 
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+			{/* Using path="*"" means "match anything", so this route
+					acts like a catch-all for URLs that we don't have explicit
+					routes for. */}
+			<Route path="*" element={<Error />} />
+			</Route>
+		</Routes>
+		</div>
+	);
 }
 
 function Home() {
-  return (
-    <div>
-      <h1> Page d'accueil 🏠</h1>
-    </div>
-  );
+	return (
+		<HomeWrapper>
+			<HomerContainer>
+				<LeftCol>
+					<StyledTitle>
+						Repérez vos besoins, on s’occupe du reste, avec les meilleurs
+						talents
+					</StyledTitle>
+					<StyledLink to="/survey/1" $isFullLink>
+						Faire le test
+					</StyledLink>
+				</LeftCol>
+				<Illustration src={Presentation} />
+			</HomerContainer>
+		</HomeWrapper>
+	);
 }
